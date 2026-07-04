86e29834a825dbbcbe5ad4fced9565a2097e34f0
Priyanshu <priyanshu2507.rjs@gmail.com>
2026-07-04 15:39:04 +0530

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
             `,
           }}
         />
-        <GoogleOAuthProvider clientId="675811978439-tdc597i7hnbm3i318ccumd08bgj31kji.apps.googleusercontent.com">
+        <GoogleOAuthProvider clientId="675811978439-tdc597i7hnbm3i318ccumd08bgj31kji.apps.googleusercontent.com"> 
           <SidebarProvider>{children}</SidebarProvider>
         </GoogleOAuthProvider>
       </body>
